86147df7f6fa77be1fad753420030a4afb4f6027
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-08-01 02:30:02 +0000

    backup 01/08/2019

--- daily_push.sh
+++ daily_push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd /home/ganeshvarmai4u/forge_minecraft_server
+cd /home/ganeshvarmai4u/minecraft
 DATE=`date +'%d/%m/%Y'`
 git add .
 git commit -m "backup $DATE"
-git push -f daily master
+git push -f origin master
